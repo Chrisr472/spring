f6261a00ca1c71edefbcb6ffedd33a847d0d2736
Chris Rohan M <chrisrohan472@gmail.com>
2022-03-05 16:40:37 +0530

    Summa Commit

--- src/main/java/com/example/demo/Example.java
+++ src/main/java/com/example/demo/Example.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://springboot-postgressql.herokuapp.com/")
 @RestController
 public class Example {
 	@Autowired
