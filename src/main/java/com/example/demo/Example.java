35ec7ec07d932756849286a39bcb732e067e4596
Chris Rohan M <chrisrohan472@gmail.com>
2022-03-05 14:54:34 +0530

    Summa Commit

---
+++ src/main/java/com/example/demo/Example.java
@@ -0,0 +1,56 @@
+package com.example.demo;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+public class Example {
+	@Autowired
+	private DataRepository studentRepo;
+	 
+	 @PostMapping("/students")
+    public String create( @RequestBody Data data ) {
+		
+        
+		 studentRepo.save(data);
+			
+			
+        
+       
+        return "Created";
+    }
+	 @GetMapping("/studenti")
+	 public String getById(@RequestParam(value="id") String i)
+	 {   Integer s=Integer.parseInt(i);
+		
+			Data l =studentRepo.getById(s);
+            return l.toString();
+	 }
+	 
+	 @GetMapping("/student")
+	    public List<Data> listById(Model model) {
+			
+	        
+		    List<Data> listStudents = studentRepo.findAll();
+	       
+	        return  listStudents;
+	    }
+	 @DeleteMapping("/deleteall")
+	 public ResponseEntity<String> delete(Model model)
+	 {   
+		 studentRepo.deleteAll();  
+		 return  ResponseEntity.ok("Delete");
+	 }
+	 @PutMapping("/update")
+	    public ResponseEntity<String> update(@RequestParam(value="id") String id,Model model) {
+	      Data dat=studentRepo.getById(Integer.parseInt(id));
+	      dat.setEmail("chrisrohan@gmail.com");
+	       studentRepo.save(dat);
+	        return  ResponseEntity.ok("updated");
+	        
+	    }
+
+}
